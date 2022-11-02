ad1d38d667d249391e62c4eedf19cfc2acd69166
Jibraan Asim <90691191+jibraanasimnit@users.noreply.github.com>
2022-11-02 11:41:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+console.log(process.argv)
+const input = process.argv[2];
+
 function Palindrome(input) {
-  const j = input.length - 1;
-  for (let i = 0; i < j / 2; i++) {
-    let x = input[i];
-    let y = input[j - i];
-    if (x != y) {
-      return false;
-    } else {
-      return true;
+    const len = input.length - 1;
+    for (let i = 0; i < len / 2; i++) {
+      let x = input[i];
+      let y = input[len - i];
+      if (x != y) {
+        return false;
+      } else {
+        return true;
+      }
     }
   }
-}
-
-console.log(Palindrome("abac"));
+  
+  console.log(Palindrome(input));
